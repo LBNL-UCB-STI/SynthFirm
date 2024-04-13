df5d105b920c7d80d58255357147a98085917bcf
Xiaodan Xu <arielinseu@gmail.com>
2024-04-12 17:57:53 -0700

    processing MOVES default fleet

---
+++ input_generation/fleet_generation/moves_fleet_distribution_generator.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 11:28:36 2023
+
+@author: xiaodanxu
+"""
+
+from pandas import read_csv
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+
+plt.style.use('ggplot')
+sns.set(font_scale=1.2)  # larger font
+
+os.chdir('/Users/xiaodanxu/Documents/SynthFirm.nosync')
+
+
+# load input
+
+# list_of_matrix_file = ['penn_2018_7_75_50.csv']
+path_to_moves = 'RawData/MOVES'
+
+hpms_definition = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'HPMS_definition')
+source_type_hpms = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'source_type_HPMS')
+source_type_population = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'source_type_population')
+age_distribution = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'AGE_distribution')
+fuel_type_distribution = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'fuel_type_distribution')
+fuel_type_definition = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'fuel_type_definition')
+RMAR_factor = pd.read_excel(os.path.join(path_to_moves, 'moves_definition.xlsx'), 
+                                sheet_name = 'RMAR')
+
+# <codecell>
+
+# generate VMT distribution by source type and model year
+analysis_year = 2021
+
+selected_type = [32, 52, 53, 61, 62]
+commercial_only = 1 # 1 = yes, 0 = no
+
+# select vehicle composition from the analysis year
+source_type_population_year = \
+    source_type_population.loc[source_type_population['yearID'] == analysis_year]
+source_type_population_year = source_type_population_year.drop(columns = 'yearID')
+
+if commercial_only == 1:
+    source_type_population_year = \
+        source_type_population_year.loc[source_type_population_year['sourceTypeID'].isin(selected_type)]
+    source_type_hpms = \
+        source_type_hpms.loc[source_type_hpms['sourceTypeID'].isin(selected_type)]
+        
+age_distribution_year = \
+    age_distribution.loc[age_distribution['yearID'] == analysis_year]
+age_distribution_year.loc[:, 'modelYearID'] = \
+    analysis_year - age_distribution_year.loc[:, 'ageID'] 
+
+# generate VMT fraction by vehicle type and age
+fleet_mix_by_hpms = pd.merge(source_type_hpms, hpms_definition,
+                              on = 'HPMSVtypeID', how = 'left')
+fleet_mix_by_hpms = pd.merge(fleet_mix_by_hpms, source_type_population_year,
+                              on = 'sourceTypeID', how = 'left')
+fleet_mix_by_hpms = pd.merge(fleet_mix_by_hpms, age_distribution_year,
+                              on = 'sourceTypeID', how = 'left')
+
+fleet_mix_by_hpms.loc[:, 'population_by_year'] =  \
+    fleet_mix_by_hpms.loc[:, 'sourceTypePopulation'] * \
+        fleet_mix_by_hpms.loc[:, 'ageFraction']
+fleet_mix_by_hpms = pd.merge(fleet_mix_by_hpms, RMAR_factor,
+                              on = ['sourceTypeID', 'ageID'], 
+                              how = 'left') 
+fleet_mix_by_hpms.loc[:, 'weighted_vmt_rate'] =  \
+    fleet_mix_by_hpms.loc[:, 'population_by_year'] * \
+        fleet_mix_by_hpms.loc[:, 'relativeMAR']
+        
+fleet_mix_by_hpms.loc[:, 'vmt_fraction'] =  \
+    fleet_mix_by_hpms.loc[:, 'weighted_vmt_rate'] / \
+        fleet_mix_by_hpms.loc[:, 'weighted_vmt_rate'].sum()
+fleet_mix_by_hpms = fleet_mix_by_hpms[['sourceTypeID', 
+                                        'ageID',
+                                        'HPMSVtypeID', 
+                                        'sourceTypeName', 
+                                        'HPMSVtypeName',
+                                        'sourceTypePopulation',
+                                        'ageFraction', 
+                                        'modelYearID',
+                                        'population_by_year', 
+                                        'relativeMAR',
+                                        'weighted_vmt_rate',
+                                        'vmt_fraction']]
+if commercial_only == 1:
+    fleet_mix_by_hpms.to_csv(os.path.join(path_to_moves, 'MOVES_VMT_fraction_com_only.csv'), 
+                                          index = False)
+else:
+    fleet_mix_by_hpms.to_csv(os.path.join(path_to_moves, 'MOVES_VMT_fraction_alltypes.csv'), 
+                                          index = False)
+
+
+# plot MOVES commercial vehicle fleet mix
+sns.lineplot(data=fleet_mix_by_hpms, x="ageID", y="vmt_fraction", hue="sourceTypeName")
+plt.xlabel('Vehicle Age')
+plt.ylabel('VMT fraction')
+plt.legend(fontsize = 12)
+plt.title('MOVES VMT fraction by vehicle type and age')
+plt.savefig(os.path.join(path_to_moves, 'plot/MOVES_commercial_vehicle_age_dist.png'), 
+            bbox_inches = 'tight', dpi = 300)
+plt.show()
+        
+# <codecell>
+# append fuel types
+
+'''
+MOVES specs:
+stmyFuelEngFraction - fraction of vehicles within the source type, model year, 
+fuel type combination which will be allocated to the given reg class
+
+stmyFraction - fraction of vehicles within the source type, model year combination 
+which are of the given reg class and fuel type
+
+fuelDensity - the density of the given fuel type in grams / gallon
+'''
+
+fuel_type_agg_frac = \
+    fuel_type_distribution.groupby(['sourceTypeID', 'modelYearID', 'fuelTypeID'])[['stmyFraction']].sum()
+fuel_type_agg_frac = fuel_type_agg_frac.reset_index()    
+
+fuel_type_definition = fuel_type_definition[['fuelTypeID', 'fuelTypeDesc']]
+fuel_type_agg_frac = pd.merge(fuel_type_agg_frac,
+                              fuel_type_definition,
+                              on = 'fuelTypeID',
+                              how = 'left')
+
+fleet_mix_with_fuel = pd.merge(fleet_mix_by_hpms, fuel_type_agg_frac,
+                               on = ['sourceTypeID', 'modelYearID'],
+                               how = 'left')
+
+fleet_mix_with_fuel.loc[:, 'vmt_fraction'] = \
+    fleet_mix_with_fuel.loc[:, 'vmt_fraction'] * fleet_mix_with_fuel.loc[:, 'stmyFraction']
+print(len(fleet_mix_with_fuel))
+print(fleet_mix_with_fuel.loc[:, 'vmt_fraction'].sum())
+
+if commercial_only == 1:
+    fleet_mix_with_fuel.to_csv(os.path.join(path_to_moves, 'MOVES_VMT_fraction_with_fuel_com_only.csv'), 
+                                          index = False)
+else:
+    fleet_mix_with_fuel.to_csv(os.path.join(path_to_moves, 'MOVES_VMT_fraction_with_fuel_alltypes.csv'), 
+                                          index = False)
+
+# <codecell>
+
+# result visualization
