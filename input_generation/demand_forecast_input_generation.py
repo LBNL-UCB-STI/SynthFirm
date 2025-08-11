45e128881f11010d6d2b5e4307495b8a211c838a
Xiaodan Xu <arielinseu@gmail.com>
2025-08-11 15:23:40 -0700

    prepare inputs for international trade forecast

--- input_generation/demand_forecast_input_generation.py
+++ input_generation/demand_forecast_input_generation.py
@@ -26,15 +26,15 @@ mode_lookup = {1: 'Truck', 2: 'Rail', 3: 'Other', 4: 'Air',
                5: 'Parcel', 6: 'Other', 7: 'Other', 8: 'Other'}
 
 #define scenario input
-analysis_year = 2017
+analysis_year = 2050
 shipment_load_attr = 'tons_' + str(analysis_year)
 shipment_tonmile_attr = 'tmiles_' + str(analysis_year)
 shipment_value_attr = 'value_' + str(analysis_year)
 
-# base_year = 2017
-# shipment_load_base = 'tons_' + str(base_year)
-# shipment_tonmile_base = 'tmiles_' + str(base_year)
-# shipment_value_base = 'value_' + str(base_year)
+base_year = 2017
+shipment_load_base = 'tons_' + str(base_year)
+shipment_tonmile_base = 'tmiles_' + str(base_year)
+shipment_value_base = 'value_' + str(base_year)
 
 
 # load FAF5 data
@@ -42,7 +42,10 @@ shipment_value_attr = 'value_' + str(analysis_year)
 # load FAF5 data
 
 faf_data = read_csv('validation/FAF5.6.1.csv', sep = ',')
+faf_data_import = faf_data.loc[faf_data['trade_type'] == 2]
+faf_data_export = faf_data.loc[faf_data['trade_type'] == 3]
 faf_data = faf_data.loc[faf_data['trade_type'] == 1]
+
 print(faf_data.columns)
 
 sctg_group_lookup = read_csv('SynthFirm_parameters/SCTG_Groups_revised_V2.csv', sep = ',')
@@ -58,6 +61,9 @@ faf_data = pd.merge(faf_data, sctg_group_lookup, left_on = 'sctg2',
 # faf_data.head(5)
 
 # <codecell>
+############################################################
+#### part 1 - domestic demand forecast (absolute value) ####
+############################################################
 
 # total production
 aggr_var = [shipment_load_attr, shipment_tonmile_attr, shipment_value_attr]
@@ -65,7 +71,6 @@ faf_production = faf_data.groupby(['dms_orig', 'SCTG_Code', 'SCTG_Name'])[aggr_v
 faf_production = faf_production.reset_index()
 
 converstion_factor = 10**6/(1000*2000)
-# value_density_base = 'value_density_' + str(base_year)
 value_density_attr = 'value_density_' + str(analysis_year)
 
 # faf_production.loc[:, value_density_base] = faf_production.loc[:, shipment_value_base] * \
@@ -74,25 +79,16 @@ value_density_attr = 'value_density_' + str(analysis_year)
 faf_production.loc[:, value_density_attr] = faf_production.loc[:, shipment_value_attr] * \
     converstion_factor / faf_production.loc[:, shipment_load_attr]
 
-# print(len(faf_production))
-# print(len(faf_production.dms_orig.unique()))
-# faf_production.head(5)
-
 # <codecell>
 
 # process attraction
 faf_attraction = faf_data.groupby(['dms_dest', 'SCTG_Code', 'SCTG_Name'])[aggr_var].sum()
 faf_attraction = faf_attraction.reset_index()
 
-# faf_attraction.loc[:, value_density_base] = faf_attraction.loc[:, shipment_value_base] * converstion_factor / \
-# faf_attraction.loc[:, shipment_load_base]
 
 faf_attraction.loc[:, value_density_attr] = faf_attraction.loc[:, shipment_value_attr] * converstion_factor / \
 faf_attraction.loc[:, shipment_load_attr]
 
-print(len(faf_attraction))
-print(len(faf_attraction.dms_dest.unique()))
-faf_attraction.head(5)
 # <codecell>
 # saving output
 production_output_attr = ['dms_orig', 'SCTG_Code',	shipment_load_attr, value_density_attr]
@@ -101,12 +97,14 @@ attraction_output_attr = ['dms_dest', 'SCTG_Code',	shipment_load_attr, value_den
 faf_production_output = faf_production[production_output_attr]
 faf_attraction_output = faf_attraction[attraction_output_attr]
 
-faf_production_output.to_csv('SynthFirm_parameters/total_commodity_production_' + str(analysis_year) + '.csv', sep = ',', index = False)
-faf_attraction_output.to_csv('SynthFirm_parameters/total_commodity_attraction_' + str(analysis_year) + '.csv', sep = ',', index = False)
+faf_production_output.to_csv('SynthFirm_parameters/total_commodity_production_' + str(analysis_year) + '.csv', 
+                             sep = ',', index = False)
+faf_attraction_output.to_csv('SynthFirm_parameters/total_commodity_attraction_' + str(analysis_year) + '.csv', 
+                             sep = ',', index = False)
 
 # <codecell>
 # generate unit cost
-# faf_production_output.head(5)
+
 unit_cost_forecast = faf_production_output[['dms_orig', 'SCTG_Code', value_density_attr]]
 
 unit_cost_forecast = pd.merge(unit_cost_forecast, cfs_faf_lookup,
@@ -118,4 +116,78 @@ unit_cost_forecast = \
 unit_cost_forecast.rename(columns = {'ST_MA': 'ORIG_CFS_AREA',
                                      'SCTG_Code': 'Commodity_SCTG',
                                     value_density_attr: 'UnitCost'})
-unit_cost_forecast.to_csv('SynthFirm_parameters/data_unitcost_by_zone_faf' + str(analysis_year) + '.csv', sep = ',', index = False)
+unit_cost_forecast.to_csv('SynthFirm_parameters/data_unitcost_by_zone_faf' + str(analysis_year) + '.csv', 
+                          sep = ',', index = False)
+
+
+# <codecell>
+#################################################################
+#### part 2 - International demand forecast (percent change) ####
+#################################################################
+
+# generate import growth rate (% forecast - base)
+attr_var = [shipment_load_attr, shipment_value_attr, \
+             shipment_load_base, shipment_value_base]
+group_var = ['fr_orig', 'dms_orig'] # foreign country, entry port
+
+import_projection_factor = \
+    faf_data_import.groupby(group_var)[attr_var].sum().reset_index()
+    
+import_projection_factor.loc[:, 'load_factor'] = \
+    import_projection_factor.loc[:, shipment_load_attr] / \
+        import_projection_factor.loc[:, shipment_load_base]
+
+import_projection_factor.loc[:, 'value_factor'] = \
+    import_projection_factor.loc[:, shipment_value_attr] / \
+        import_projection_factor.loc[:, shipment_value_base]
+import_projection_factor.dropna(inplace = True)
+
+print("Range of import load adjustment:")
+print(import_projection_factor.loc[:, 'load_factor'].min(),
+      import_projection_factor.loc[:, 'load_factor'].max())
+
+print("Range of import value adjustment:")
+print(import_projection_factor.loc[:, 'value_factor'].min(),
+      import_projection_factor.loc[:, 'value_factor'].max())
+
+output_attr = group_var.copy()
+output_attr.extend(['load_factor', 'value_factor'])
+import_projection_factor = import_projection_factor[output_attr]
+
+# <codecell>
+
+# generate export growth rate (% forecast - base)
+
+group_var = ['dms_dest', 'fr_dest'] # foreign country, entry port
+
+export_projection_factor = \
+    faf_data_export.groupby(group_var)[attr_var].sum().reset_index()
+    
+export_projection_factor.loc[:, 'load_factor'] = \
+    export_projection_factor.loc[:, shipment_load_attr] / \
+        export_projection_factor.loc[:, shipment_load_base]
+
+export_projection_factor.loc[:, 'value_factor'] = \
+    export_projection_factor.loc[:, shipment_value_attr] / \
+        export_projection_factor.loc[:, shipment_value_base]
+export_projection_factor.dropna(inplace = True)
+
+print("Range of import load adjustment:")
+print(export_projection_factor.loc[:, 'load_factor'].min(),
+      export_projection_factor.loc[:, 'load_factor'].max())
+
+print("Range of import value adjustment:")
+print(export_projection_factor.loc[:, 'value_factor'].min(),
+      export_projection_factor.loc[:, 'value_factor'].max())
+
+output_attr = group_var.copy()
+output_attr.extend(['load_factor', 'value_factor'])
+export_projection_factor = export_projection_factor[output_attr]
+
+# <codecell>
+
+# write international output
+import_projection_factor.to_csv('SynthFirm_parameters/factor_import_' + str(analysis_year) + '.csv', 
+                             sep = ',', index = False)
+export_projection_factor.to_csv('SynthFirm_parameters/factor_export_' + str(analysis_year) + '.csv', 
+                             sep = ',', index = False)
---
+++ input_generation/port_inputs/international_flow_from_faf_usato.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 11 14:29:51 2025
+
+@author: xiaodanxu
+"""
+# set environment and import packages
+import os
+from pandas import read_csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import shapely.wkt
+import geopandas as gpd
+import contextily as cx
+import warnings
+
+warnings.filterwarnings("ignore")
+
+os.chdir('/Users/xiaodanxu/Documents/SynthFirm.nosync')
+
+'''
+Notes:
+    >FAF data is used as spatial allocation factor, and could vary by year
+    >USATO data provide base year flow by port, provide benchmark values, 
+     and will be projected for future years use scenario-specific inputs.
+     Therefore, the input USATO value does not vary by year.
+'''
+# mapping FAF mode to SynthFirm mode
+mode_lookup = {1: 'Truck', 2: 'Rail', 3: 'Other', 4: 'Air', 
+               5: 'Parcel', 6: 'Other', 7: 'Other', 8: 'Other'}
+
+#define scenario input
+analysis_year = 2050 # for FAF inputs
+us_ton_to_ton = 0.907185
+miles_to_km = 1.60934
+shipment_load_attr = 'tons_' + str(analysis_year)
+shipment_tonmile_attr = 'tmiles_' + str(analysis_year)
+shipment_value_attr = 'value_' + str(analysis_year)
+region_name = 'Seattle'
+out_scenario_name = 'Seattle'
+region_code = [531, 532, 539, 411] # seattle
+# region_code = [62, 64, 65, 69] #Bay Area
+
+path_to_write = 'inputs_' + region_name # update this based on analysis region 
+path_to_plot = 'plots_' + out_scenario_name # update this based on analysis region
+faf_data = read_csv('Validation/' + 'FAF5.6.1.csv', sep = ',')
+faf_zone_file = 'Validation/FAF5Zones.geojson'
+faf_zone = gpd.read_file(faf_zone_file)
+faf_zone.plot()
+
+crosswalk_file = 'SynthFirm_parameters/international_trade_zone_lookup.csv'
+international_zonal_crosswalk = read_csv(crosswalk_file)
+
+
+col_to_keep = ['fr_orig', 'dms_orig', 'dms_dest', 'fr_dest', 'fr_inmode', 'dms_mode',
+       'fr_outmode', 'sctg2', 'trade_type', 'dist_band', shipment_load_attr,
+               shipment_value_attr, shipment_tonmile_attr]
+faf_data = faf_data[col_to_keep]
+faf_val_by_trade_type = \
+faf_data.groupby('trade_type')[[shipment_load_attr,
+        shipment_value_attr, shipment_tonmile_attr]].sum()
+print(faf_val_by_trade_type)
+
+faf_data = faf_data.loc[faf_data['trade_type'] != 1]
+
+# <codecell>
+# generate import/export flows from FAF
+
+faf_crosswalk = \
+international_zonal_crosswalk[['FAFID',	'CFS_CODE','CFS_NAME']]
+
+faf_crosswalk = faf_crosswalk.drop_duplicates()
+
+# FAF import attributes  
+regional_import = faf_data.loc[faf_data['trade_type'] == 2]
+regional_import = \
+regional_import.loc[regional_import['dms_orig'].isin(region_code)]
+
+regional_import['fr_orig'] = regional_import['fr_orig'].astype(int)
+
+faf_crosswalk['FAFID'] = faf_crosswalk['FAFID'].astype(int)
+
+regional_import = pd.merge(regional_import, faf_crosswalk,
+                           left_on = 'fr_orig', 
+                           right_on = 'FAFID', how = 'left')
+regional_import = regional_import.drop(columns = 'FAFID')
+regional_import.loc[:, 'distance'] = \
+1000 * regional_import.loc[:, shipment_tonmile_attr] / \
+    regional_import.loc[:, shipment_load_attr]
+
+print('total import within selected region (load, value, tmile):')
+print(regional_import[[shipment_load_attr,
+        shipment_value_attr, shipment_tonmile_attr]].sum())
+
+
+# FAF export attributes  
+regional_export = faf_data.loc[faf_data['trade_type'] == 3]
+regional_export = \
+regional_export.loc[regional_export['dms_dest'].isin(region_code)]
+
+regional_export['dms_dest'] = regional_export['dms_dest'].astype(int)
+
+regional_export = pd.merge(regional_export, faf_crosswalk,
+                           left_on = 'fr_dest', 
+                           right_on = 'FAFID', how = 'left')
+regional_export = regional_export.drop(columns = 'FAFID')
+
+regional_export.loc[:, 'distance'] = \
+1000 * regional_export.loc[:, shipment_tonmile_attr] / \
+    regional_export.loc[:, shipment_load_attr]
+print('total export within selected region (load, value, tmile):')
+print(regional_export[[shipment_load_attr,
+        shipment_value_attr, shipment_tonmile_attr]].sum())
+
+# <codecell>
+# create mode split results for validation
+mode_lookup = {1: 'Truck', 2: 'Rail', 3: 'Other', 4: 'Air', 
+               5: 'Parcel', 6: 'Other', 7: 'Other', 8: 'Other'}
+regional_import.loc[:, 'mode_def'] = \
+regional_import.loc[:, 'dms_mode'].map(mode_lookup)
+
+regional_export.loc[:, 'mode_def'] = \
+regional_export.loc[:, 'dms_mode'].map(mode_lookup)
+
+# mode split by tonnage
+import_mode_split = \
+regional_import.groupby('mode_def')[[shipment_load_attr]].sum()
+import_mode_split = import_mode_split.reset_index()
+import_mode_split.loc[:, 'direction'] = 'import'
+export_mode_split = \
+regional_export.groupby('mode_def')[[shipment_load_attr]].sum()
+export_mode_split = export_mode_split.reset_index()
+export_mode_split.loc[:, 'direction'] = 'export'
+mode_split_faf = pd.concat([import_mode_split, export_mode_split] )
+
+mode_split_faf.loc[:, 'fraction'] = \
+mode_split_faf.loc[:, shipment_load_attr] / \
+mode_split_faf.groupby(['direction'])[shipment_load_attr].transform('sum')
+mode_split_faf.to_csv(os.path.join(path_to_write, 'port', 
+                                   'FAF_mode_split_' + str(analysis_year)+'.csv'),
+                      index = False)
+
+# <codecell>
+# write to SynthFirm inputs
+regional_import.to_csv(os.path.join(path_to_write, 'port', 
+                                    'FAF_regional_import_'+ str(analysis_year)+'.csv'),
+                      index = False)
+
+regional_export.to_csv(os.path.join(path_to_write, 'port', 
+                                    'FAF_regional_export_'+ str(analysis_year)+'.csv'),
+                      index = False)
+
+# <codecell>
+
+# process USATO data
+# load USATO data
+port_file = 'RawData/Port/port_code_cbp.csv'
+list_of_ports = read_csv(port_file)
+
+port_loc_file = 'SynthFirm_parameters/port_location_CA_WA_OR.geojson'
+port_locations = gpd.read_file(port_loc_file)
+
+import_flow_file = 'RawData/Port/Flow/SF and Seattle Port-level Imports (2017)_with OR.csv'
+regional_import_flow = read_csv(import_flow_file)
+
+export_flow_file = 'RawData/Port/Flow/SF and Seattle Port-level Exports (2017)_with OR.csv'
+regional_export_flow = read_csv(export_flow_file)
+
+study_region_definition = region_name + '_freight.geojson'
+study_region = gpd.read_file(os.path.join(path_to_write, study_region_definition))
+
+# <codecell>
+
+# processing port shapefile/boundary
+
+list_of_ports.loc[:, 'PORTID'] = \
+list_of_ports.loc[:, 'CBP Port Location'].str.split('(').str[0]
+list_of_ports.loc[:, 'PORTID'] = \
+list_of_ports.loc[:, 'PORTID'].str[:-1]
+
+port_locations_with_id = pd.merge(port_locations,
+                                  list_of_ports,
+                                  on = 'PORTID',
+                                  how = 'left')
+print(len(port_locations_with_id))
+
+port_locations_with_id_df = port_locations_with_id.drop(columns = 'geometry')
+port_locations_centroid = port_locations_with_id["geometry"].centroid
+port_locations_centroid = \
+gpd.GeoDataFrame(geometry = gpd.GeoSeries(port_locations_centroid))
+port_locations_centroid = pd.concat([port_locations_centroid, 
+                               port_locations_with_id_df], axis = 1)
+port_locations_centroid.head(5)
+faf_zone = faf_zone.to_crs('EPSG:4326')
+
+# assign port to faf zone
+port_in_faf = \
+port_locations_centroid.sjoin_nearest(faf_zone, how="left")
+
+port_in_faf['FAF'] = port_in_faf['FAF'].astype(int)
+port_in_faf.drop(columns = ['index_right'], inplace = True)
+port_in_region = \
+port_in_faf.loc[port_in_faf['FAF'].isin(region_code)]
+study_region = study_region.to_crs('EPSG:4326')
+
+port_in_region = \
+port_in_region.sjoin_nearest(study_region, how="left")
+port_in_region.drop(columns = ['index_right'], inplace = True)
+port_in_region[['lat', 'lon']] = \
+    port_in_region.apply(lambda p: (p.geometry.y, p.geometry.x), axis=1, 
+                        result_type='expand')
+    
+port_in_region_df = port_in_region.drop(columns = 'geometry')
+
+port_in_region.to_file(os.path.join(path_to_write, 'Port/port_location_in_region.geojson'),
+                   driver="GeoJSON")
+
+port_in_region_df.to_csv(os.path.join(path_to_write, 'Port/port_location_in_region.csv'),
+                   index = False)
